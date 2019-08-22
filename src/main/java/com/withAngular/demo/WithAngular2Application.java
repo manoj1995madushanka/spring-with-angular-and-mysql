0b336918de627d38159515bb83da7de4e4912825
manoj1995madushanka <manoj@codegen.net>
2019-08-22 13:58:32 +0530

    init

---
+++ src/main/java/com/withAngular/demo/WithAngular2Application.java
@@ -0,0 +1,13 @@
+package com.withAngular.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithAngular2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithAngular2Application.class, args);
+	}
+
+}
