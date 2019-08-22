21fc86fb5f20cdaacfb04144d4522c4ce41b8644
manoj1995madushanka <manoj@codegen.net>
2019-08-22 14:29:07 +0530

    create responce body to print simple message

--- src/main/java/com/withAngular/demo/player/PlayerController.java
+++ src/main/java/com/withAngular/demo/player/PlayerController.java
@@ -1,5 +1,16 @@
 package com.withAngular.demo.player;
 
-public class PlayerController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class PlayerController {
+	
+	
+	@RequestMapping(value = "/message") // localhost:8080/message
+	@ResponseBody // because controller only accept by this type of data formats
+	public String getMessage() {
+		return "Hi there";
+	}
 }
